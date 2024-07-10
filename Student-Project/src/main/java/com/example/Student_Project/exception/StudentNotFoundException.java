2f346a1164d151f6290d9d9c8958783629767ff1
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-07-10 12:48:14 +0400

    Create StudentNotFoundException class that extend exception in Spring Project

---
+++ Student-Project/src/main/java/com/example/Student_Project/exception/StudentNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.Student_Project.exception;
+
+public class StudentNotFoundException extends Exception {
+    public StudentNotFoundException() {
+        super("Student Not Found");
+    }
+}
